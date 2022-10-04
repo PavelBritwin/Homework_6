61428f2e8c3089f0e102d57891ab7419bbb57d70
Pavel Anokhin <anohinpa@gmail.com>
2022-10-04 20:03:20 +0300

    Task 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 41
+
+Console.WriteLine("Укажите количество чисел для ввода: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int[m];
+int positiveCount = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите число № {i}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0) positiveCount++;
+}
+Console.WriteLine($"Количество введенных положительных чисел: {positiveCount}");
